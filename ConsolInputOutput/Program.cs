b835eaeca6660584324134ee2835eb5d52cc9729
AlexandrTolstov <alextav_85@mail.ru>
2019-01-04 17:30:18 +0600

    1

--- ConsolInputOutput/Program.cs
+++ ConsolInputOutput/Program.cs
@@ -10,6 +10,20 @@ namespace ConsolInputOutput
     {
         static void Main(string[] args)
         {
+            Console.Title = "Пример использования инструментов класса Console";
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            int lenght = ("Input Encoding: " + Console.InputEncoding.ToString()).Length + 1;
+            Console.SetWindowSize(lenght, 8);
+            Console.SetBufferSize(lenght, 8);
+            Console.WriteLine("Input Encoding: " + Console.InputEncoding.ToString());
+            Console.WriteLine("Output Encoding: " + Console.OutputEncoding.ToString());
+            Console.ResetColor();
+            Console.WriteLine("Is NUM LOCK turned on: " + Console.NumberLock.ToString());
+            Console.WriteLine("Is Caps LOCK turned on: " + Console.CapsLock.ToString());
+            Console.Write("Enter a simple message:");
+            string message = Console.ReadLine();
+            Console.WriteLine("Your message is: " + message);
         }
     }
 }
